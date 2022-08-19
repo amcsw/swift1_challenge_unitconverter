c086fd3b9e9fd953fef26b2b948bfbc2f5b4ffc3
amcsw <aldo.chocos@amcsoftware.pe>
2022-08-19 12:03:40 -0500

    challenge completed!

--- swift1-challenge-unitconversion/ContentView.swift
+++ swift1-challenge-unitconversion/ContentView.swift
@@ -2,15 +2,90 @@
 //  ContentView.swift
 //  swift1-challenge-unitconversion
 //
-//  Created by Aldo Chocos on 19/08/22.
+//  Created by AMC Software on 19/08/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var temperatureFrom = "Celsius"
+    @State private var temperatureTo = "Farenheit"
+    @State private var temperatureValue: Double = 0
+    
+    var finalResult: Double {
+        
+        if temperatureFrom == "Celsius" && temperatureTo == "Farenheit" {
+            return temperatureValue * 9/5 + 32.0
+        } else
+        if temperatureFrom == "Farenheit" && temperatureTo == "Celsius" {
+            return (temperatureValue - 32.0) * 5/9
+        } else
+        if temperatureFrom == "Celsius" && temperatureTo == "Kelvin" {
+            return (temperatureValue + 273.15)
+        } else
+        if temperatureFrom == "Kelvin" && temperatureTo == "Celsius" {
+            return (temperatureValue - 273.15)
+        } else
+        if temperatureFrom == "Farenheit" && temperatureTo == "Kelvin" {
+            return (temperatureValue + 459.67) * 5/9
+        } else
+        if temperatureFrom == "Kelvin" && temperatureTo == "Farenheit" {
+            return (temperatureValue * 9/5) - 459.67
+        }
+        
+        return temperatureValue
+    }
+    
+    let tempertatureOptions = ["Celsius", "Farenheit","Kelvin"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            
+            Form {
+                Section {
+                    
+                    Picker("From", selection: $temperatureFrom) {
+                        ForEach(tempertatureOptions, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("From")
+                }
+                
+                Section {
+                    Picker("From", selection: $temperatureTo) {
+                        ForEach(tempertatureOptions, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("To")
+                }
+                
+                Section {
+                    TextField("Value",
+                              value: $temperatureValue,
+                              format: .number
+                    )
+                    .keyboardType(.decimalPad)
+                } header: {
+                    Text("Enter value")
+                }
+                
+                Section {
+                    Text(finalResult, format: .number)
+                } header: {
+                    Text("Result")
+                }
+            }
+            .navigationTitle("Temperature converter")
+        }
+        
     }
 }
 
