3be63dac3a7afa3ad19a4bf898ead54026289e26
amcsw <aldo.chocos@amcsoftware.pe>
2022-08-19 11:02:36 -0500

    Initial Commit

---
+++ swift1-challenge-unitconversion/swift1_challenge_unitconversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift1_challenge_unitconversionApp.swift
+//  swift1-challenge-unitconversion
+//
+//  Created by Aldo Chocos on 19/08/22.
+//
+
+import SwiftUI
+
+@main
+struct swift1_challenge_unitconversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
